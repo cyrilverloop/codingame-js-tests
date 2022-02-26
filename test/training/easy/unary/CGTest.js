ab7de1f4a3aa0543cd3090cd6fe6279608056201
Cyril VERLOOP <cyril.verloop@webqard.fr>
2022-02-26 12:47:18 +0100

    Adding tests for "Unary".

---
+++ test/training/easy/unary/CGTest.js
@@ -0,0 +1,65 @@
+import { assert } from 'chai';
+import sinon from 'sinon';
+import File from '../../../File.js';
+import { execute } from '../../../../lib/training/easy/unary/unary.js';
+
+/**
+ * Tests for the "Unary" puzzle.
+ */
+suite('Unary', function() {
+    const sandbox = sinon.createSandbox();
+
+    setup(function () {
+        sandbox.stub(console, "log");
+    });
+
+    teardown(function () {
+        sandbox.restore();
+    });
+
+
+    test('can execute "Character C"', function() {
+        let inputFile = new File('./test/training/easy/unary/input/01 - character C.txt');
+
+        execute(inputFile.readline.bind(inputFile));
+
+        assert.strictEqual(
+            console.log.getCall(0).args[0],
+            '0 0 00 0000 0 00'
+        );
+    });
+
+    test('can execute "Message CC"', function() {
+        let inputFile = new File('./test/training/easy/unary/input/02 - message CC.txt');
+
+        execute(inputFile.readline.bind(inputFile));
+
+        assert.strictEqual(
+            console.log.getCall(0).args[0],
+            '0 0 00 0000 0 000 00 0000 0 00'
+        );
+    });
+
+    test('can execute "Character %"', function() {
+        let inputFile = new File('./test/training/easy/unary/input/03 - character %.txt');
+
+        execute(inputFile.readline.bind(inputFile));
+
+        assert.strictEqual(
+            console.log.getCall(0).args[0],
+            '00 0 0 0 00 00 0 0 00 0 0 0'
+        );
+    });
+
+    test('can execute "Message from Chuck Norris"', function() {
+        let inputFile = new File('./test/training/easy/unary/input/04 - message from Chuck Norris.txt');
+        let outputFile = new File('./test/training/easy/unary/output/04 - message from Chuck Norris.txt');
+
+        execute(inputFile.readline.bind(inputFile));
+
+        assert.strictEqual(
+            console.log.getCall(0).args[0],
+            outputFile.readline()
+        );
+    });
+});
