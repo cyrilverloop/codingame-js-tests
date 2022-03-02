9d3e82639b91e1b4ec0de5e5f26631beb7f2cfab
Cyril VERLOOP <cyril.verloop@webqard.fr>
2022-03-02 15:09:29 +0100

    Fixing output answer assertion

--- test/assertOutputAnswer.js
+++ test/assertOutputAnswer.js
@@ -3,16 +3,47 @@ import File from './File.js';
 
 function assertOutputAnswer(fileName) {
     let outputFile = new File(fileName);
+    let userAnswer = '';
     let line = outputFile.readline();
-    let answerIndex = 0;
+    let answer = "";
+
+    for(const [consoleCallIndex, consoleCall] of Object.entries(console.log.getCalls())) {
+        userAnswer += consoleCall.args[0] + "\n";
+    }
 
     while(line !== "false") {
-        assert.strictEqual(
-            console.log.getCall(answerIndex).args[0],
-            line
-        );
+        answer += line + "\n";
         line = outputFile.readline();
-        answerIndex++;
+    }
+
+    userAnswer = removeTraillingNewLine(userAnswer);
+
+    assert.strictEqual(
+        userAnswer,
+        answer
+    );
+}
+
+function removeTraillingNewLine(answer) {
+    let index = answer.length - 1;
+
+    while(answer.charAt(index) === "\n") {
+        index--;
+    }
+
+    return answer.slice(0, (index + 2));
+}
+
+function removeTraillingNewLineBAK(answer) {
+
+    if(
+        answer.charAt(answer.length - 1) === "\n"
+        && answer.charAt(answer.length - 2) === "\n"
+    ) {
+        return removeTraillingNewLine(answer.slice(0, answer.length - 1));
+    }
+    else {
+        return answer;
     }
 }
 
