05a9f12adfb8404d1d93c9cb92a902e9e57233f9
Cyril VERLOOP <cyril.verloop@webqard.fr>
2022-03-06 14:41:14 +0100

    Adding tests for "ISBN check digit".

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 - Tests for "Walk on a die".
 - Tests for "Dolbear's law".
+- Tests for "ISBN check digit".
 
 ### Changed
 - Using "__dirname" for input / output paths.
---
+++ test/community/training/easy/ISBNCheckDigit/CGTest.js
@@ -0,0 +1,55 @@
+import { assert } from 'chai';
+import sinon from 'sinon';
+import File from '../../../../File.js';
+import { assertOutputAnswer } from '../../../../assertOutputAnswer.js';
+import { execute } from '../../../../../lib/community/training/easy/ISBNCheckDigit/ISBNCheckDigit.js';
+
+const __dirname = new URL('.', import.meta.url).pathname;
+
+/**
+ * Tests for the "ISBN check digit" puzzle.
+ */
+suite('ISBN check digit', function() {
+    const sandbox = sinon.createSandbox();
+
+    setup(function () {
+        sandbox.stub(console, "log");
+    });
+
+    teardown(function () {
+        sandbox.restore();
+    });
+
+
+    test('Example', function() {
+        let inputFile = new File(__dirname + 'input/01 - example.txt');
+
+        execute(inputFile.readline.bind(inputFile));
+
+        assertOutputAnswer(__dirname + 'output/01 - example.txt');
+    });
+
+    test('Short', function() {
+        let inputFile = new File(__dirname + 'input/02 - short.txt');
+
+        execute(inputFile.readline.bind(inputFile));
+
+        assertOutputAnswer(__dirname + 'output/02 - short.txt');
+    });
+
+    test('Longer', function() {
+        let inputFile = new File(__dirname + 'input/03 - longer.txt');
+
+        execute(inputFile.readline.bind(inputFile));
+
+        assertOutputAnswer(__dirname + 'output/03 - longer.txt');
+    });
+
+    test('Much longer', function() {
+        let inputFile = new File(__dirname + 'input/04 - much longer.txt');
+
+        execute(inputFile.readline.bind(inputFile));
+
+        assertOutputAnswer(__dirname + 'output/04 - much longer.txt');
+    });
+});
