d8996819c1f666adf389e55e82c67a4554db01d5
Cyril VERLOOP <cyril.verloop@webqard.fr>
2022-04-21 00:27:56 +0200

    Adding tests for "Dead men's shot".

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -26,6 +26,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Tests for "The mystic rectangle".
 - Tests for "The travelling salesman problem".
 - Tests for "Murder in the village!".
+- Tests for "Dead men's shot".
 
 ## [1.7.0] - 2022-03-31
 ### Added
--- test/community/training/easy/deadMensShot/CGTest.js
+++ test/community/training/easy/deadMensShot/CGTest.js
@@ -2,11 +2,11 @@ import { assert } from 'chai';
 import sinon from 'sinon';
 import File from '../../../../File.js';
 import { assertOutputAnswer } from '../../../../assertOutputAnswer.js';
-import { execute } from '../../../../../lib/community/training/easy/creditCardVerifier/creditCardVerifier.js';
+import { execute } from '../../../../../lib/community/training/easy/deadMensShot/deadMensShot.js';
 
 const __dirname = new URL('.', import.meta.url).pathname;
 
-suite('Container terminal', function() {
+suite("Dead men's shot", function() {
     const sandbox = sinon.createSandbox();
 
     setup(function () {
@@ -18,35 +18,43 @@ suite('Container terminal', function() {
     });
 
 
-    test('Tests', function() {
-        let inputFile = new File(__dirname + 'input/01 - tests.txt');
+    test("Square", function() {
+        let inputFile = new File(__dirname + 'input/01 - square.txt');
 
         execute(inputFile.readline.bind(inputFile));
 
-        assertOutputAnswer(__dirname + 'output/01 - tests.txt');
+        assertOutputAnswer(__dirname + 'output/01 - square.txt');
     });
 
-    test('MasterCard', function() {
-        let inputFile = new File(__dirname + 'input/02 - MasterCard.txt');
+    test("Triangle", function() {
+        let inputFile = new File(__dirname + 'input/02 - triangle.txt');
 
         execute(inputFile.readline.bind(inputFile));
 
-        assertOutputAnswer(__dirname + 'output/02 - MasterCard.txt');
+        assertOutputAnswer(__dirname + 'output/02 - triangle.txt');
     });
 
-    test('Discover', function() {
-        let inputFile = new File(__dirname + 'input/03 - Discover.txt');
+    test("Five corners", function() {
+        let inputFile = new File(__dirname + 'input/03 - five corners.txt');
 
         execute(inputFile.readline.bind(inputFile));
 
-        assertOutputAnswer(__dirname + 'output/03 - Discover.txt');
+        assertOutputAnswer(__dirname + 'output/03 - five corners.txt');
     });
 
-    test('VISA', function() {
-        let inputFile = new File(__dirname + 'input/04 - VISA.txt');
+    test("Six sides", function() {
+        let inputFile = new File(__dirname + 'input/04 - six sides.txt');
 
         execute(inputFile.readline.bind(inputFile));
 
-        assertOutputAnswer(__dirname + 'output/04 - VISA.txt');
+        assertOutputAnswer(__dirname + 'output/04 - six sides.txt');
+    });
+
+    test("Ten sides", function() {
+        let inputFile = new File(__dirname + 'input/05 - ten sides.txt');
+
+        execute(inputFile.readline.bind(inputFile));
+
+        assertOutputAnswer(__dirname + 'output/05 - ten sides.txt');
     });
 });
